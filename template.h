d587b1addcc295d6a535344db8307d0344d1b058
leticialopes <l184423@dac.unicamp.br>
2023-12-07 13:17:37 -0300

    adicionando template

---
+++ template.h
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pair<int, int>> vpii;
+typedef vector<pair<char, int>> vpci;
+typedef pair<int, int> pii;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef map<char, int> mci;
+typedef map<string, int> msi;
+typedef set<int> si;
+typedef set<ll> sll;
+typedef set<char> sc;
+typedef set<string> ss;
+
+#define endl '\n'
+#define f first
+#define s second
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define all(c) (c).begin(), (c).end()
+#define lbnd lower_bound
+#define ubnd upper_bound
+#define debug cerr << "\ni'm here\n"  << endl;
+// #define PI 3.141592653589793
+#define PI acos(-1.0)
+#define EPS 1e-9 // 0.000000001
+
+const int MAX_N = 1000000007;
