86eb1da69ce9029990a725f41274bf08b1d85c40
luisademelo14 <l238001@dac.unicamp.br>
2023-12-07 14:30:52 -0300

    bfsssssssssssssss

--- GRAPHS/breadth_first_search.cpp
+++ GRAPHS/breadth_first_search.cpp
@@ -1,17 +1,34 @@
-// #include <bits/stdc++.h>
- 
-// using namespace std;
-
-// typedef long long ll;
-
-#include <../template.h>
+#include "../template.h"
 
 vector<vector<int>> g; // grafo como lista de adj
 
 int n; // numero de vertices
 
-int s; // vertice do inicio da bfs
+int v0; // vertice do inicio da bfs
 
 queue<int> q; // fila usada pro algoritmo
 
 vector<bool> visited;
+
+vector<int> d(n); // vetor com as distancias minimas
+
+vector<int> p(n); // vetor com os vertices anteriores
+
+void bfs(){
+    q.push(v0);
+    visited[v0] = true;
+    p[v0] = -1;
+    while(!q.empty()){
+        int v = q.front(); // v eh o vertice atual
+        q.pop();
+        for(int u : g[v]){ // passando pelos vizinhos de v
+            if(!visited[u]){
+                visited[u] = true;
+                q.push(u);
+                // atualizando caminho minimo entre v0 e u
+                d[u] = d[v] + 1;
+                p[u] = v;
+            }
+        }
+    }
+}
